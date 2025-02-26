db97f43d059be89c57d8825c5df5d7d1a5a275f9
Supriya <42831113+Sip21@users.noreply.github.com>
2025-02-26 19:15:35 +0530

    Delete MyFirstServlet directory

--- MyFirstServlet/src/com/my/project/MyFirstClass.java
+++
@@ -1,9 +0,0 @@
-package com.my.project;
-
-public class MyFirstClass {
-
-	public static void main(String[] args){
-			String str = "Supriya"; 
-			System.out.println("My Name is:: " + str);
-	}
-}
--- MyFirstServlet/src/com/my/project/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Supriya
- *
- */
-package com.my.project;
