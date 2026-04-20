ad9fcb93b48d5a5c3f23ebee014d5da73da1d8b7
Supriya <supriyas11d@gmail.com>
2026-04-20 21:40:34 +0530

    Commit for polymorphism

---
+++ FunWithJava/src/com/funwithjava/simple/Cat.java
@@ -0,0 +1,10 @@
+package com.funwithjava.simple;
+
+public class Cat extends Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("Cat does meow meow !!");
+    }
+
+}
---
+++ FunWithJava/src/com/funwithjava/simple/Dog.java
@@ -0,0 +1,10 @@
+package com.funwithjava.simple;
+
+public class Dog extends Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("Dog does woof woof !!");
+    }
+
+}
